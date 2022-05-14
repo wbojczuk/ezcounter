549eda5b4b2e11ead0f62bcd82453e8adb59b837
William Bojczuk <williambojczuk@gmail.com>
2022-05-13 20:53:25 -0400

    just an update

---
+++ ezcounter.js
@@ -0,0 +1,63 @@
+
+let EZCounter = {
+
+    // START EZSCRIPT
+    setup: () => {
+       
+            const textContainer = document.querySelectorAll(".EZCounter");
+            textContainer.forEach((container)=>{
+                container.style.position = "relative";
+                const textArea = container.querySelector("textarea");
+                textArea.style.width = "100%";
+                textArea.style.height = "90%";
+                textArea.style.resize = "none";
+                textArea.addEventListener("input", ()=>{EZCounter.check()});
+                const outputElem = document.createElement("div");
+                outputElem.style.position = "absolute";
+                outputElem.style.display = "flex";
+                outputElem.style.alignItems = "center";
+                outputElem.style.justifyContent = "right";
+                outputElem.style.bottom = "0";
+                outputElem.style.left= "0";
+                outputElem.style.backgroundColor = "#ccc";
+                outputElem.style.width= "100%";
+                outputElem.style.height = "10%";
+                outputElem.style.border = "1px solid #aaa";
+                outputElem.className = "EZOutput";
+                outputElem.textContent = "Words:";
+                const tempSpan = document.createElement("span");
+                tempSpan.style.paddingRight = "5%";
+                tempSpan.style.paddingLeft = "1%";
+                outputElem.appendChild(tempSpan.cloneNode(false));
+                container.append(outputElem.cloneNode(true))
+            });
+    
+    
+    
+       
+   
+
+    },
+    check:()=>{
+        
+        const textAreas = document.querySelectorAll(".EZCounter textarea");
+        const areasLength = textAreas.length;
+        for(let i=0; i<areasLength;i++){
+            let currentText = textAreas[i].value;
+
+            let  wordCount = 0;
+            if(/[a-z0-9]{1,}[.]*/gi.test(currentText)){
+                wordCount = currentText.match(/[a-z0-9]{1,}[.]*/gi).length;
+            }
+            
+            textAreas[i].parentElement.querySelector(".EZOutput span").textContent = wordCount;
+            
+            
+
+        }
+    }
+    // END EZSCRIPT
+};
+
+EZCounter.setup();
+EZCounter.check();
